ee86f984584fa1126a7809c10c54eabfecaf583c
khalf <mohamed.khalaf@cowpay.me>
2024-03-13 01:46:02 +0200

    enqueue style using wp_enqueue_scripts

--- wp-content/themes/aquila/functions.php
+++ wp-content/themes/aquila/functions.php
@@ -5,3 +5,9 @@
  *
  *
  */
+
+function aquila_enqueue_scripts()
+{
+    wp_enqueue_style( 'stylesheet',get_stylesheet_uri(), false, '1.1', 'all');
+}
+add_action('wp_enqueue_scripts','aquila_enqueue_scripts');
