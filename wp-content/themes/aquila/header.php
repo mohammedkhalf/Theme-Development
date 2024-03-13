22badaab3c84b0df426660138204a2e97b9bad43
khalf <mohamed.khalaf@cowpay.me>
2024-03-13 22:33:22 +0200

    add bootstraped header using get tempalte part

--- wp-content/themes/aquila/header.php
+++ wp-content/themes/aquila/header.php
@@ -25,5 +25,10 @@
 
 ?>
 
+<div id="page" class="site">
+    <header id="masthead" class="site-header" role="banner">
+        <?php  get_template_part('template-parts/header/nav'); ?>
+    </header>
+    <div id="content" class="site-content">
 
-<header>Header</header>
+
